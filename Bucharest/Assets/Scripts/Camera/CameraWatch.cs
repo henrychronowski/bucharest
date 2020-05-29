3532b462b04254c9a3d40bf8640e449c4459beaa
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2020-05-29 15:58:13 -0400

    Adding initial test cameras

---
+++ Bucharest/Assets/Scripts/Camera/CameraWatch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWatch : MonoBehaviour
+{
+	[SerializeField] private Transform target;
+
+	private void LateUpdate()
+	{
+		transform.LookAt(target);
+	}
+}
