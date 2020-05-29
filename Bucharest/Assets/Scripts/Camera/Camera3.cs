eb5d6d8c3dfd9004aaf1829bb3bf325153d27c88
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2020-05-29 16:48:44 -0400

    Adding somewhat free-targeting camera, no collisions yet

--- Bucharest/Assets/Scripts/Camera/Camera3.cs
+++ Bucharest/Assets/Scripts/Camera/Camera3.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Follows the player at offset with horizontal rotation guided by mouse and no damping
+
 public class Camera3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] Transform target;
+	[SerializeField] Vector3 offset = Vector3.zero;
+	[SerializeField] float rotateSpeed = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void LateUpdate()
+	{
+		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+		target.Rotate(0, horizontal, 0);
+
+		float desiredAngle = target.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+		transform.position = target.position - (rotation * offset);
+
+		transform.LookAt(target);
+	}
 }
---
+++ Bucharest/Assets/Scripts/Camera/Camera4.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Freely ish moving camera
+
+public class Camera4 : MonoBehaviour
+{
+	[SerializeField] Transform target;
+	[SerializeField] float offset = 0.0f;
+	[SerializeField] float damping = 0.0f;
+	[SerializeField] float minTurnAngle = -90.0f;
+	[SerializeField] float maxTurnAngle = 0.0f;
+	[SerializeField] float turnSpeed = 4.0f;
+
+	private float rotX = 0.0f;
+
+	private void LateUpdate()
+	{
+		float y = Input.GetAxis("Mouse X") * turnSpeed;
+		rotX += Input.GetAxis("Mouse Y") * turnSpeed;
+
+		rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+
+		transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0.0f);
+		transform.position = target.position - (transform.forward * offset);
+
+		transform.LookAt(target);
+	}
+}
