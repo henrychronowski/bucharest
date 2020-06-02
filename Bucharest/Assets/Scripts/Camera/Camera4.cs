76b606c086f202b5acb525aa432454a850dbb61b
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2020-06-02 13:58:51 -0400

    Adding smoothed collisions

--- Bucharest/Assets/Scripts/Camera/Camera4.cs
+++ Bucharest/Assets/Scripts/Camera/Camera4.cs
@@ -7,24 +7,48 @@ using UnityEngine;
 public class Camera4 : MonoBehaviour
 {
 	[SerializeField] Transform target;
-	[SerializeField] float offset = 0.0f;
+	[SerializeField] float minOffset = 1.0f;
+	[SerializeField] float maxOffset = 4.0f;
 	[SerializeField] float damping = 0.0f;
 	[SerializeField] float minTurnAngle = -90.0f;
 	[SerializeField] float maxTurnAngle = 0.0f;
-	[SerializeField] float turnSpeed = 4.0f;
+	[SerializeField] Vector2 turnSpeed = new Vector2(4.0f, 4.0f);
 
 	private float rotX = 0.0f;
+	private float offset;
+	private float prevOffset;
+
+	private void Awake()
+	{
+		prevOffset = maxOffset;
+
+		//NB: This line should not be in this file
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
 	private void LateUpdate()
 	{
-		float y = Input.GetAxis("Mouse X") * turnSpeed;
-		rotX += Input.GetAxis("Mouse Y") * turnSpeed;
+		RaycastHit hit;
+		float y = Input.GetAxis("Mouse X") * turnSpeed.x;
+		rotX += Input.GetAxis("Mouse Y") * turnSpeed.y;
 
 		rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
-
 		transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0.0f);
-		transform.position = target.position - (transform.forward * offset);
 
+		if(Physics.Linecast(target.position, transform.position, out hit))
+		{
+			offset = Mathf.Clamp(hit.distance, minOffset, maxOffset);
+		}
+		else
+		{
+			offset = maxOffset;
+		}
+
+		offset = Mathf.Lerp(prevOffset, offset, Time.deltaTime * damping);
+
+		transform.position = target.position - (transform.forward * offset);
 		transform.LookAt(target);
+
+		prevOffset = offset;
 	}
 }
