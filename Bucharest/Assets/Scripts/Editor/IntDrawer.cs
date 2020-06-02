b9a8be2f9e7d87d12be184617fe85ad84d961d8b
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2020-06-02 14:48:15 -0400

    Adding sample int property drawer

---
+++ Bucharest/Assets/Scripts/Editor/IntDrawer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(int))]
+public class IntDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		EditorGUI.BeginProperty(position, label, property);
+		position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+		GUI.backgroundColor = Color.green;
+		EditorGUI.PropertyField(position, property, GUIContent.none);
+
+		EditorGUI.EndProperty();
+	}
+}
