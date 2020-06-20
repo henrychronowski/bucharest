4ed0e1c69c2f309c4a637a5b1931d37035c02c1f
Ethan Heil <eheil20@gmail.com>
2020-06-20 15:58:02 -0400

    Creating shader for the minimap. Starting to work on render delay for minimap.

---
+++ Bucharest/Assets/Scripts/Camera/MinimapCameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Camera renderCamera;
+    [SerializeField] float delay = 0.5f;
+    [SerializeField] RenderTexture renderTexture;
+
+
+    private void Awake()
+    {
+        renderCamera.targetTexture = renderTexture;
+
+        StartCoroutine(Render());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Render()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            Debug.Log(renderCamera.activeTexture);
+            renderCamera.activeTexture.Release();
+            renderCamera.targetTexture = renderTexture;
+        } 
+    }
+}
--- Bucharest/Assets/Scripts/Editor/IntDrawer.cs
+++ Bucharest/Assets/Scripts/Editor/IntDrawer.cs
@@ -4,21 +4,21 @@
  * Purpose: A custom int property drawer
  */
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
 
-[CustomPropertyDrawer(typeof(int))]
-public class IntDrawer : PropertyDrawer
-{
-	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-	{
-		EditorGUI.BeginProperty(position, label, property);
-		position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-		GUI.backgroundColor = Color.green;
-		EditorGUI.PropertyField(position, property, GUIContent.none);
+//[CustomPropertyDrawer(typeof(int))]
+//public class IntDrawer : PropertyDrawer
+//{
+//	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+//	{
+//		EditorGUI.BeginProperty(position, label, property);
+//		position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+//		GUI.backgroundColor = Color.green;
+//		EditorGUI.PropertyField(position, property, GUIContent.none);
 
-		EditorGUI.EndProperty();
-	}
-}
+//		EditorGUI.EndProperty();
+//	}
+//}
